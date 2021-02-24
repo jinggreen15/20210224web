6e65e318dc31700e165b46ecb10e156eba3d798c
jinggreen15 <jinggreen15@gmail.com>
2021-02-24 21:30:46 +0800

    20210224

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 // 安裝jQuery Code Snippets套件
 // 輸入jqe 選 jqeachElement
-$(selector).each(function (index, element) {
-    // element == this
-    
+$("a").each(function (index, element) {
+
+    console.log("編號 : " + index);
+
+    var target = $(this).attr("data-st-target");
+    var duration = $(this).attr("data-st-target");
+
+    console.log("目標 : " + target);
+    console.log("時間 : " + duration);
+
+    // 輸入套件jqo 選擇jqoffsetGet
+    var offset = $("#" + target).offset();
+    var top = offset.top;
+
+    console.log("上方 : " + top);
+
+    // 輸入套件 jqc 選擇jqchange
+    $(this).change(function (e) {
+        e.preventDefault();
+
+        // parseInt = 將文字轉為數字
+        $("html").animate({
+            scrollTop: top
+        }, parseInt(duration));
+    });
+
 });
+
