494acc37acbabd154c23cb29fa967e033ec23f6d
jinggreen15 <jinggreen15@gmail.com>
2021-03-01 20:29:35 +0800

    20210301

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 // 安裝jQuery Code Snippets套件
 // 輸入jqe 選 jqeachElement
+
+// 搜尋 a 元素
+// 取得每個 a 元素的資料
 $("a").each(function (index, element) {
 
     console.log("編號 : " + index);
 
     var target = $(this).attr("data-st-target");
     var duration = $(this).attr("data-st-target");
+    var offsetTop = $(this).attr("data-st-offset");
 
     console.log("目標 : " + target);
     console.log("時間 : " + duration);
+    console.log("位移：" + offsetTop);
+
+    var offset = $("#" + target).offset();
+    var top = offset.top;
 
     // 輸入套件jqo 選擇jqoffsetGet
     var offset = $("#" + target).offset();
@@ -28,3 +36,7 @@ $("a").each(function (index, element) {
 
 });
 
+// 箭頭顯示與隱藏效果
+$(window).scroll(function() {
+    
+})
